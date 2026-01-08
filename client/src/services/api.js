45de333d731a8c928f465707ece241196b2a1320
Abhinand Babu K <abhinandkbabu@gmail.com>
2026-01-08 12:58:11 +0530

    api url currected

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://coupon-app-e0nq.onrender.com",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://coupon-app-e0nq.onrender.com",
 
 });
 
